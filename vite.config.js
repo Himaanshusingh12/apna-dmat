33bac7c0f4664399196d117e5f96a1f7c7928598
Himaanshusingh12 <himaanshu1601@gmail.com>
2025-04-16 10:51:23 +0530

    new config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,30 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: './',
+//   plugins: [react()],
+// })
+
+// new one
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true,
+  }
 })
 
