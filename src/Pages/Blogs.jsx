faf24baf09508ea3b0ed9a409928dba1f69b3d0c
Himaanshusingh12 <himaanshu1601@gmail.com>
2025-04-21 11:50:35 +0530

    blog change

--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import { BACKEND_URL } from "../Constant";
 import { NavLink } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 function Blogs() {
   const [blog, Setblog] = useState([]);
@@ -24,10 +25,6 @@ function Blogs() {
         // console.log("The fetched Active blog are", response.data);
       }
     } catch (error) {
-      // console.error(
-      //   "Error fetching Blog category:",
-      //   error.response?.data || error.message
-      // );
       toast.error(
         `Error fetching Blog category: ${
           error.response?.data?.message || error.message
@@ -35,8 +32,40 @@ function Blogs() {
       );
     }
   };
+
+  // section for fetch meta title , meta description and meta keywords
+  const [seoDetail, SetseoDetail] = useState([]);
+
+  useEffect(() => {
+    fetchSeodetail();
+  }, []);
+
+  const fetchSeodetail = async () => {
+    try {
+      const response = await axios.get(`${BACKEND_URL}/api/seo/get-active`);
+      if (response.status === 200) {
+        SetseoDetail(response.data.data);
+        console.log("The Fetched seo detail are:", response.data);
+      }
+    } catch (error) {
+      toast.error(
+        `Error fetching seo detail: ${
+          error.response?.data?.message || error.message
+        }`
+      );
+    }
+  };
+
+  const blogSeo = seoDetail.find((item) => item.page_name === "blog");
   return (
     <>
+      {blogSeo && (
+        <Helmet>
+          <title>{blogSeo.meta_title}</title>
+          <meta name="description" content={blogSeo.meta_description} />
+          <meta name="keywords" content={blogSeo.meta_keywords} />
+        </Helmet>
+      )}
       <TopBar />
       <Header pageTitle="Blogs" breadcrumb1="Blogs" />
       <div className="container py-5">
