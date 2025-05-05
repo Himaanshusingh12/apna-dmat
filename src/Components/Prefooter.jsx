741a94dc67d508d70055a3aa03ea1c4be3c6e7bf
Himaanshusingh12 <himaanshu1601@gmail.com>
2025-05-05 10:53:12 +0530

    prefooter

--- src/Components/Prefooter.jsx
+++ src/Components/Prefooter.jsx
@@ -1,28 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import "../Styles/Navbar.css";
 function Prefooter() {
   return (
     <>
-      <section className="container py-5 my-3 border-start border-4 border-warning bg-white shadow-sm">
+      <section className="container py-5 my-3 border-start border-4 bg-white shadow-sm left-border">
         <div className="row align-items-center">
-          <div className="col-md-8">
+          <div className="col-md-8 ps-5">
             <h2 className="fw-bold text-dark">
               Let's Build Your Financial Future
-              <span className="text-success">Together</span>
+              <span className="together-button">Together</span>
             </h2>
             <p className="text-muted mb-0">
               From SIPs and equities to insurance and FDs — our team is ready to
               guide you every step of the way. Don't wait to secure your future.
             </p>
           </div>
-          <div className="col-md-4 text-md-end mt-4 mt-md-0">
-            <Link
-              href="/contact"
-              className="btn btn-warning text-white fw-bold px-4 py-2 rounded-pill shadow"
+          <div className="col-md-4 text-md-end mt-4 mt-md-0 pe-5 ">
+            <a
+              href="https://chat.whatsapp.com/LcuZGSxH3Tj5WQefAWkqTy"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn-warning text-white fw-bold px-4 py-2 rounded-pill chats-button"
             >
               Let's Chat
-            </Link>
+            </a>
           </div>
         </div>
       </section>
